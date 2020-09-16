78fdb4bf9f6e668caf4da8ef0e7ba9e296feca70
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-09-16 16:02:42 +0700

    thuc hanh He cac doi tuong hinh hoc

--- src/SquareTest.java
+++ src/SquareTest.java
@@ -3,7 +3,7 @@ public class SquareTest {
         Square square = new Square();
         System.out.println(square);
 
-        square = new Square(2.3);
+        square = new Square(2.4);
         System.out.println(square);
 
         square = new Square(5.8, "yellow", true);
